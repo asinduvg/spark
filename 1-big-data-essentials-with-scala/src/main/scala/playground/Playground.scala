522702ab8b4d71921c61caaff9ec318bbf430fa7
Asindu <ashvg.dev@gmail.com>
2022-01-13 20:56:05 +0530

    data frames basics

--- 1-big-data-essentials-with-scala/src/main/scala/playground/Playground.scala
+++ 1-big-data-essentials-with-scala/src/main/scala/playground/Playground.scala
@@ -42,31 +42,20 @@ object Playground extends App {
   /**
    * A "manual" sequence of rows describing cars, fetched from cars.json in the data folder.
    */
-  val cars = Seq(
-    Row("chevrolet chevelle malibu",18.0,8L,307.0,130L,3504L,12.0,"1970-01-01","USA"),
-    Row("buick skylark 320",15.0,8L,350.0,165L,3693L,11.5,"1970-01-01","USA"),
-    Row("plymouth satellite",18.0,8L,318.0,150L,3436L,11.0,"1970-01-01","USA"),
-    Row("amc rebel sst",16.0,8L,304.0,150L,3433L,12.0,"1970-01-01","USA"),
-    Row("ford torino",17.0,8L,302.0,140L,3449L,10.5,"1970-01-01","USA"),
-    Row("ford galaxie 500",15.0,8L,429.0,198L,4341L,10.0,"1970-01-01","USA"),
-    Row("chevrolet impala",14.0,8L,454.0,220L,4354L,9.0,"1970-01-01","USA"),
-    Row("plymouth fury iii",14.0,8L,440.0,215L,4312L,8.5,"1970-01-01","USA"),
-    Row("pontiac catalina",14.0,8L,455.0,225L,4425L,10.0,"1970-01-01","USA"),
-    Row("amc ambassador dpl",15.0,8L,390.0,190L,3850L,8.5,"1970-01-01","USA")
-  )
+
 
   /**
    * The two lines below create an RDD of rows (think of an RDD like a parallel collection).
    * Then from the RDD we create a DataFrame, which has a number of useful querying methods.
    */
-  val carsRows = sc.parallelize(cars)
-  val carsDF = spark.createDataFrame(carsRows, carsSchema)
+//  val carsRows = sc.parallelize(cars)
+//  val carsDF = spark.createDataFrame(carsRows, carsSchema)
 
   /**
    * If the schema and the contents of the DataFrame are printed correctly to the console,
    * this means the libraries work correctly and you can jump into the course!
    */
-  carsDF.printSchema()
-  carsDF.show()
+//  carsDF.printSchema()
+//  carsDF.show()
 
 }
