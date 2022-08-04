5fc48e724f3bf9ebcbd0d8d0bbca04285f732a01
Asindu <ashvg.dev@gmail.com>
2022-08-04 08:42:49 +0530

    event time windows

---
+++ 2-apache-spark-streaming-3-with-scala/src/main/scala/part6advanced/EventTimeWindows.scala
@@ -0,0 +1,71 @@
+package part6advanced
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType, TimestampType}
+
+object EventTimeWindows {
+
+  val spark = SparkSession.builder()
+    .appName("Event Time Windows")
+    .master("local[2]")
+    .getOrCreate()
+
+  val onlinePurchaseSchema = StructType(Array(
+    StructField("id", StringType),
+    StructField("time", TimestampType),
+    StructField("item", StringType),
+    StructField("quantity", IntegerType)
+  ))
+
+  def readPurchasesFromSocket() = spark.readStream
+    .format("socket")
+    .option("host", "localhost")
+    .option("port", 12345)
+    .load()
+    .select(from_json(col("value"), onlinePurchaseSchema).as("purchase"))
+    .selectExpr("purchase.*")
+
+  def aggregatePurchasesByTumblingWindow() = {
+    val purchasesDF = readPurchasesFromSocket()
+
+    val windowByDay = purchasesDF
+      .groupBy(window(col("time"), "1 day").as("time")) // tumbling window: sliding duration == window duration
+      .agg(sum("quantity").as("totalQuantity"))
+      .select(
+        col("time").getField("start").as("start"),
+        col("time").getField("end").as("end"),
+        col("totalQuantity")
+      )
+
+    windowByDay.writeStream
+      .format("console")
+      .outputMode("complete")
+      .start()
+      .awaitTermination()
+  }
+
+  def aggregatePurchasesBySlidingWindow() = {
+    val purchasesDF = readPurchasesFromSocket()
+
+    val windowByDay = purchasesDF
+      .groupBy(window(col("time"), "1 day", "1 hour").as("time")) // struct column: has fields {start, end}
+      .agg(sum("quantity").as("totalQuantity"))
+      .select(
+        col("time").getField("start").as("start"),
+        col("time").getField("end").as("end"),
+        col("totalQuantity")
+      )
+
+    windowByDay.writeStream
+      .format("console")
+      .outputMode("complete")
+      .start()
+      .awaitTermination()
+  }
+
+  def main(args: Array[String]): Unit = {
+    aggregatePurchasesBySlidingWindow()
+  }
+
+}
